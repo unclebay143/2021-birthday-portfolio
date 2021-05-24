01f6bb0dbadce27c6b752dd0791a4122b01adea3
unclebay <unclebigbay@gmail.com>
2021-05-24 02:57:55 +0100

    text contact form display none hire button on smaller devices

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -28,7 +28,14 @@ const sliderNav = (manual) =>{
 
     // remove active from other slides
     slides.forEach((slide) => {
+        // OTHER SLIDES
         slide.classList.remove("active")
+
+
+        // TOGGLE THE HAMBURGER ALSO
+        menuBtn.classList.toggle("active")
+        // CLOSE THE NAVIGATION WHEN USER CLICK A NAVLINK
+        navigation.classList.remove("active")
     });
 
     // remove active from other contents
@@ -48,5 +55,12 @@ const sliderNav = (manual) =>{
 btns.forEach((btn, i) => {
     btn.addEventListener("click", ()=>{
         sliderNav(i)
+
+        // DISPLAY MEDIA ICON ON LANDING PAGE
+        if(i === 0){
+            document.querySelector(".hide-media-icons").classList.replace("hide-media-icons", "media-icons")
+        }else if((i === 1 || i === 2 || i === 4) && window.screen.availWidth < 900){// HIDE ICONS FOR PROJECT, CERTIFICATE AND CONTACT PAGE ON SMALL DEVICES
+            document.querySelector(".media-icons").classList.replace("media-icons", "hide-media-icons")
+        }
     })
 });
